5b8d6df42980da98d68880a3ed1f4a9737ddc2da
zarrasofea <zarrasofea07@gmail.com>
2025-06-23 00:56:41 +0800

    new

--- projectweb/staffSchedule.php
+++ projectweb/staffSchedule.php
@@ -69,11 +69,11 @@ $serviceRef = $conn->query("SELECT * FROM service");
     <p><em>View your schedule and click “DONE” as a sign that you’ve completed your task.</em></p>
   </div>
 
-  <div class="search-bar-container">
+  <!-- Search Order Section -->
+<div class="search-section">
   <form method="GET" class="search-form">
     <label class="search-label">Search</label>
     <input type="text" name="search" placeholder="Order ID" value="<?= $_GET['search'] ?? '' ?>" />
-    <button type="submit" class="search-icon">🔍</button>
   </form>
 </div>
 
